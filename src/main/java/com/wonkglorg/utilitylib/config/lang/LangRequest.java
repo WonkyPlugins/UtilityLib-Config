ab18406c181be5d97064f943921b958dd2bcfe28
Wonkglorg <Wonkglorg+github@protonmail.com>
2026-05-15 12:42:33 +0200

    Rewrote Lang Requests to allow conditionals and simplified logic

--- src/main/java/com/wonkglorg/utilitylib/config/lang/LangRequest.java
+++ src/main/java/com/wonkglorg/utilitylib/config/lang/LangRequest.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,6 +26,8 @@ import java.util.stream.Collectors;
  */
 @SuppressWarnings("unused")
 public class LangRequest{
+	
+	private static final Pattern CONDITIONAL_PATTERN = Pattern.compile("<if:(!?)([a-zA-Z0-9_-]+)>(.*?)((<else>(.*?))?)</if>", Pattern.DOTALL);
 	private final Logger logger;
 	/**
 	 * The lang manager this request was returned by
@@ -45,19 +46,15 @@ public class LangRequest{
 	 */
 	private final String defaultValue;
 	/**
+	 * Any registered conditionals that should be parsed by the lang
+	 */
+	private final Map<String, Boolean> conditionals = new HashMap<>();
+	/**
 	 * Map of all replacements applied to this request
 	 */
 	private final Map<String, String> replacements = new HashMap<>();
 	
 	private final Map<String, Component> componentReplacements = new HashMap<>();
-	/**
-	 * The initial returned result without any other modifications
-	 */
-	private final List<String> initialResult;
-	/**
-	 * The current result made after all values are collected
-	 */
-	private List<String> result;
 	/**
 	 * Pattern for component replacers
 	 */
@@ -73,12 +70,20 @@ public class LangRequest{
 		this.locale = locale;
 		this.key = key;
 		this.defaultValue = defaultValue;
-		this.initialResult = getValue(this.locale, key, defaultValue);
-		this.result = initialResult;
 	}
 	
 	public LangRequest forceLocale(boolean forceLocale) {
 		this.forceLocale = forceLocale;
+		return this;
+	}
+	
+	/**
+	 * Adds a conditional value to the request resolver
+	 *
+	 * @param key the key of the condition
+	 */
+	public LangRequest conditional(String key, boolean value) {
+		conditionals.put(key.toLowerCase(Locale.ROOT), value);
 		return this;
 	}
 	
@@ -90,11 +95,6 @@ public class LangRequest{
 			replacement = "";
 		}
 		replacements.put(value, replacement);
-		for(int i = 0; i < result.size(); i++){
-			String input = result.get(i);
-			input = input.replace(value, replacement);
-			result.set(i, input);
-		}
 		return this;
 	}
 	
@@ -147,180 +147,18 @@ public class LangRequest{
 		return this;
 	}
 	
-	public List<String> getUnmodifiedResult() {
-		return initialResult;
-	}
-	
-	public Locale getLocale() {
-		return locale;
+	/**
+	 * @return the raw value found in the config without any modifications
+	 */
+	public List<String> getRawResult() {
+		return getRawResult(locale);
 	}
 	
 	/**
-	 * Updates the input with all replacements found in the populated replacement map
-	 *
-	 * @param inputs to modify
-	 * @return modified input
+	 * @return the raw value found in the config without any modifications
 	 */
-	private List<String> updateResult(List<String> inputs) {
-		
-		for(int i = 0; i < inputs.size(); i++){
-			String input = inputs.get(i);
-			for(var replacement : replacements.entrySet()){
-				input = input.replace(replacement.getKey(), replacement.getValue());
-			}
-			inputs.set(i, input);
-		}
-		
-		return inputs;
-	}
-	
-	public List<String> getResult() {
-		return result;
-	}
-	
-	private List<Component> toComponent(Function<String, Component> toComponent, List<String> results) {
-		List<Component> components = new ArrayList<>();
-		if(componentReplacements.isEmpty()){
-			for(var resultValue : results){
-				components.add(toComponent.apply(resultValue));
-			}
-			
-			return components;
-		}
-		
-		if(pattern == null){
-			pattern = Pattern.compile(componentReplacements.keySet().stream().map(Pattern::quote).collect(Collectors.joining("|")));
-		}
-		for(var resultValue : results){
-			Matcher matcher = pattern.matcher(resultValue);
-			
-			List<Component> subComponents = new ArrayList<>();
-			int last = 0;
-			
-			while(matcher.find()){
-				if(matcher.start() > last){
-					subComponents.add(toComponent.apply(resultValue.substring(last, matcher.start())));
-				}
-				
-				String key = matcher.group();
-				subComponents.add(componentReplacements.getOrDefault(key, toComponent.apply(key)));
-				
-				last = matcher.end();
-			}
-			
-			if(last < resultValue.length()){
-				subComponents.add(toComponent.apply(resultValue.substring(last)));
-			}
-			
-			components.add(Component.join(JoinConfiguration.noSeparators(), subComponents));
-			
-		}
-		
-		return components;
-	}
-	
-	private Component toSingleComponent(Function<String, Component> toComponent, List<String> resultValue) {
-		String first = resultValue.getFirst();
-		if(componentReplacements.isEmpty()){
-			return toComponent.apply(first);
-		}
-		
-		if(pattern == null){
-			pattern = Pattern.compile(componentReplacements.keySet().stream().map(Pattern::quote).collect(Collectors.joining("|")));
-		}
-		Matcher matcher = pattern.matcher(first);
-		
-		List<Component> subComponents = new ArrayList<>();
-		int last = 0;
-		
-		while(matcher.find()){
-			if(matcher.start() > last){
-				subComponents.add(toComponent.apply(first.substring(last, matcher.start())));
-			}
-			
-			String key = matcher.group();
-			subComponents.add(componentReplacements.getOrDefault(key, toComponent.apply(key)));
-			
-			last = matcher.end();
-		}
-		
-		if(last < first.length()){
-			subComponents.add(toComponent.apply(first.substring(last)));
-		}
-		
-		return Component.join(JoinConfiguration.noSeparators(), subComponents);
-	}
-	
-	/**
-	 *
-	 * @param toComponent the converter to use
-	 * @return the output as a component
-	 */
-	public List<Component> toComponent(Function<String, Component> toComponent) {
-		return toComponent(toComponent, result);
-	}
-	
-	/**
-	 * @return the output as a component
-	 */
-	public List<Component> toComponent() {
-		return toComponent(MiniMessage.miniMessage()::deserialize, result);
-	}
-	
-	/**
-	 * @param toComponent the component function
-	 * @return the first line of the config defined value as a component (this cuts off any other lines also specified in the same key, should be used for lines that only have one value it can have.
-	 */
-	public Component toSingleComponent(Function<String, Component> toComponent) {
-		return toSingleComponent(toComponent, result);
-	}
-	
-	/**
-	 * @return the first line of the config defined value as a component (this cuts off any other lines also specified in the same key, should be used for lines that only have one value it can have.
-	 */
-	public Component toSingleComponent() {
-		return toSingleComponent(MiniMessage.miniMessage()::deserialize, result);
-	}
-	
-	/**
-	 * Sends the request's result to the given audience using the MiniMessage formatting.
-	 *
-	 * @param audience if the audience is a {@link Player} requests their locale to modify the message with unless {@link #forceLocale} is set to true.
-	 */
-	public void sendToAudience(@NotNull Audience audience) {
-		sendToAudience(audience, MiniMessage.miniMessage()::deserialize);
-	}
-	
-	/**
-	 * Sends the request's result to the given audience using the MiniMessage formatting.
-	 *
-	 * @param audience if the audience is a {@link Player} requests their locale to modify the message with unless {@link #forceLocale} is set to true.
-	 * @param toComponent the function to use turning the result into a component to send
-	 */
-	public void sendToAudience(@NotNull Audience audience, Function<String, Component> toComponent) {
-		if(audience instanceof Player player){
-			if(player.locale() == locale || forceLocale || langManager.getAllLangs().size() == 1){
-				toComponent(toComponent, this.result).forEach(audience::sendMessage);
-			} else {
-				this.result = updateResult(getValue(this.locale, key, defaultValue));
-				toComponent(toComponent, this.result).forEach(audience::sendMessage);
-			}
-		} else {
-			toComponent(toComponent, this.result).forEach(audience::sendMessage);
-		}
-	}
-	
-	/**
-	 * Gets a value from the language file with global replacements applied
-	 *
-	 * @param locale the locale to get the value from
-	 * @param key the key to get by
-	 * @param defaultValue the default value to return if no value was found
-	 * @return the returned result or the value if no result was found
-	 */
-	private List<String> getValue(@Nullable final Locale locale, @NotNull final String key, @Nullable String defaultValue) {
+	public List<String> getRawResult(Locale locale) {
 		LangConfig config;
-		defaultValue = defaultValue != null ? defaultValue : key;
 		var configOptional = langManager.getAnyValidLangConfig(locale);
 		if(configOptional.isPresent()){
 			config = configOptional.get();
@@ -335,44 +173,259 @@ public class LangRequest{
 			config.updateReplacerMap();
 		}
 		
+		if(!config.contains(key)){
+			List<String> arrayList = new ArrayList<>();
+			arrayList.add(defaultValue);
+			return arrayList;
+		}
+		
 		if(config.isList(key)){
-			List<String> results = config.getStringList(key);
-			
-			for(int i = 0; i < results.size(); i++){
-				String edited = results.get(i);
-				
-				for(var entry : langManager.getReplacerMap().entrySet()){
-					edited = edited.replace(entry.getKey(), entry.getValue());
-				}
-				
-				for(var entry : config.getReplacerMap().entrySet()){
-					edited = edited.replace(entry.getKey(), entry.getValue());
-				}
-				
-				results.set(i, edited);
-			}
-			
-			return results;
-			
+			return config.getStringList(key);
 		} else {
-			String resultString = config.getString(key, defaultValue);
-			if(langManager.getReplacerMap().isEmpty() && config.getReplacerMap().isEmpty()){
-				List<String> strings = new ArrayList<>();
-				strings.add(resultString);
-				return strings;
-			}
-			
-			for(var entry : langManager.getReplacerMap().entrySet()){
-				resultString = resultString.replace(entry.getKey(), entry.getValue());
-			}
-			
-			for(var entry : config.getReplacerMap().entrySet()){
-				resultString = resultString.replace(entry.getKey(), entry.getValue());
-			}
-			List<String> strings = new ArrayList<>();
-			strings.add(resultString);
-			return strings;
+			List<String> arrayList = new ArrayList<>();
+			arrayList.add(config.getString(key, defaultValue));
+			return arrayList;
 		}
 	}
 	
+	/**
+	 * @return the string or first entry of the list from the config without any modifications
+	 */
+	public String getRawResultSingleLine() {
+		return getRawResultSingleLine(locale);
+	}
+	
+	/**
+	 * @return the string or first entry of the list from the config without any modifications
+	 */
+	public String getRawResultSingleLine(Locale locale) {
+		LangConfig config;
+		var configOptional = langManager.getAnyValidLangConfig(locale);
+		if(configOptional.isPresent()){
+			config = configOptional.get();
+		} else {
+			logger.log(Level.INFO, "No lang file could be loaded for request: " + key + " using default value!");
+			return defaultValue;
+		}
+		
+		if(config.isUpdateRequest()){
+			config.updateReplacerMap();
+		}
+		
+		if(!config.contains(key)){
+			return defaultValue;
+		}
+		
+		if(config.isList(key)){
+			return config.getStringList(key).getFirst();
+		} else {
+			return config.getString(key, defaultValue);
+		}
+	}
+	
+	/**
+	 * Resolves any conditionals and placeholders in the input string (does not resolve component replacements)
+	 */
+	public String processRawResult(String toResolve) {
+		toResolve = applyConditionals(toResolve);
+		return applyPlaceHolders(toResolve);
+	}
+	
+	/**
+	 * Resolves any conditionals and placeholders in the input (does not resolve component replacements)
+	 */
+	public List<String> processRawResult(List<String> toResolve) {
+		toResolve.replaceAll(this::processRawResult);
+		return toResolve;
+	}
+	
+	public Locale getLocale() {
+		return locale;
+	}
+	
+	/**
+	 * Updates the input with all replacements found in the populated replacement map
+	 *
+	 * @param input to modify
+	 * @return modified input
+	 */
+	private String applyPlaceHolders(String input) {
+		//global replacers
+		for(var replacement : langManager.getReplacerMap().entrySet()){
+			input = input.replace(replacement.getKey(), replacement.getValue());
+		}
+		if(replacements.isEmpty()){
+			return input;
+		}
+		//per request replacers
+		for(var replacement : replacements.entrySet()){
+			input = input.replace(replacement.getKey(), replacement.getValue());
+		}
+		return input;
+	}
+	
+	/**
+	 * Resolved all component placeholders and merges them into one component output
+	 *
+	 * @param toComponent the function to convert components with
+	 * @param value the input value
+	 * @return the constructed component
+	 */
+	private Component applyComponentPlaceholders(Function<String, Component> toComponent, String value) {
+		Matcher matcher = pattern.matcher(value);
+		
+		List<Component> subComponents = new ArrayList<>();
+		int last = 0;
+		
+		while(matcher.find()){
+			if(matcher.start() > last){
+				subComponents.add(toComponent.apply(value.substring(last, matcher.start())));
+			}
+			
+			String componentKey = matcher.group();
+			subComponents.add(componentReplacements.getOrDefault(componentKey, toComponent.apply(componentKey)));
+			
+			last = matcher.end();
+		}
+		
+		if(last < value.length()){
+			subComponents.add(toComponent.apply(value.substring(last)));
+		}
+		return Component.join(JoinConfiguration.noSeparators(), subComponents);
+	}
+	
+	/**
+	 * Resolves any conditionals found in the result
+	 *
+	 * @param input the raw result to process
+	 * @return resolved string
+	 */
+	private String applyConditionals(String input) {
+		if(conditionals.isEmpty()){ //no conditions to evaluate
+			return input;
+		}
+		Matcher matcher = CONDITIONAL_PATTERN.matcher(input);
+		StringBuilder builder = new StringBuilder();
+		
+		while(matcher.find()){
+			boolean negate = !matcher.group(1).isEmpty();
+			String conditionKey = matcher.group(2).toLowerCase(Locale.ROOT);
+			
+			String trueValue = matcher.group(3);
+			String falseValue = matcher.group(6);
+			
+			boolean condition = conditionals.getOrDefault(conditionKey, false);
+			
+			if(negate){
+				condition = !condition;
+			}
+			
+			String replacement = condition ? trueValue : (falseValue != null ? falseValue : "");
+			
+			matcher.appendReplacement(builder, Matcher.quoteReplacement(replacement));
+		}
+		
+		matcher.appendTail(builder);
+		
+		return builder.toString();
+	}
+	
+	/**
+	 *
+	 * @param toComponent the converter to use
+	 * @return the output as a component
+	 */
+	private List<Component> toComponent(Function<String, Component> toComponent) {
+		return toComponent(toComponent, locale);
+	}
+	
+	/**
+	 *
+	 * @param toComponent the converter to use
+	 * @return the output as a component
+	 */
+	private List<Component> toComponent(Function<String, Component> toComponent, Locale locale) {
+		List<Component> components = new ArrayList<>();
+		List<String> results = processRawResult(getRawResult(locale));
+		if(componentReplacements.isEmpty()){
+			for(var resultValue : results){
+				components.add(toComponent.apply(resultValue));
+			}
+			return components;
+		}
+		
+		if(pattern == null){
+			pattern = Pattern.compile(componentReplacements.keySet().stream().map(Pattern::quote).collect(Collectors.joining("|")));
+		}
+		
+		for(var resultValue : results){
+			components.add(applyComponentPlaceholders(toComponent, resultValue));
+		}
+		return components;
+	}
+	
+	/**
+	 * @param toComponent the component function
+	 * @return the first line of the config defined value as a component (this cuts off any other lines also specified in the same key, should be used for lines that only have one value it can have.
+	 */
+	private Component toSingleComponent(Function<String, Component> toComponent) {
+		return toSingleComponent(toComponent, locale);
+	}
+	
+	/**
+	 * @param toComponent the component function
+	 * @return the first line of the config defined value as a component (this cuts off any other lines also specified in the same key, should be used for lines that only have one value it can have.
+	 */
+	private Component toSingleComponent(Function<String, Component> toComponent, Locale locale) {
+		String rawResult = processRawResult(getRawResultSingleLine(locale));
+		if(componentReplacements.isEmpty()){
+			return toComponent.apply(rawResult);
+		}
+		
+		if(pattern == null){
+			pattern = Pattern.compile(componentReplacements.keySet().stream().map(Pattern::quote).collect(Collectors.joining("|")));
+		}
+		return applyComponentPlaceholders(toComponent, rawResult);
+	}
+	
+	/**
+	 * @return the output as a component
+	 */
+	
+	public List<Component> toComponent() {
+		return toComponent(MiniMessage.miniMessage()::deserialize);
+	}
+	
+	/**
+	 * @return the first line of the config defined value as a component (this cuts off any other lines also specified in the same key, should be used for lines that only have one value it can have.
+	 */
+	
+	public Component toSingleComponent() {
+		return toSingleComponent(MiniMessage.miniMessage()::deserialize);
+	}
+	
+	/**
+	 * Sends the request's result to the given audience using the MiniMessage formatting.
+	 *
+	 * @param audience if the audience is a {@link Player} requests their locale to modify the message with unless {@link #forceLocale} is set to true.
+	 */
+	
+	public void sendToAudience(@NotNull Audience audience) {
+		sendToAudience(audience, MiniMessage.miniMessage()::deserialize);
+	}
+	
+	/**
+	 * Sends the request's result to the given audience using the MiniMessage formatting.
+	 *
+	 * @param audience if the audience is a {@link Player} requests their locale to modify the message with unless {@link #forceLocale} is set to true.
+	 * @param toComponent the function to use turning the result into a component to send
+	 */
+	
+	public void sendToAudience(@NotNull Audience audience, Function<String, Component> toComponent) {
+		if(audience instanceof Player player && !forceLocale){
+			toComponent(toComponent, player.locale()).forEach(audience::sendMessage);
+		} else {
+			toComponent(toComponent).forEach(audience::sendMessage);
+		}
+	}
 }
---
+++ src/test/java/com/wonkglorg/utilitylib/config/lang/LangRequestTest.java
@@ -0,0 +1,140 @@
+package com.wonkglorg.utilitylib.config.lang;
+
+import com.wonkglorg.utilitylib.config.LangManager;
+import com.wonkglorg.utilitylib.config.types.LangConfig;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+public class LangRequestTest{
+	
+	@Test
+	public void canReturnRawSingleInput() {
+		LangManager langManager = Mockito.mock(LangManager.class);
+		LangConfig config = Mockito.mock(LangConfig.class);
+		LangRequest langRequest = new LangRequest(langManager, Locale.ENGLISH, "test-key", "Nothing Found");
+		
+		//return element found
+		Mockito.when(langManager.getAnyValidLangConfig(Locale.ENGLISH)).thenReturn(Optional.of(config));
+		Mockito.when(config.contains("test-key")).thenReturn(true);
+		Mockito.when(config.getString("test-key", "Nothing Found")).thenReturn("Found Value");
+		Assert.assertEquals("Found Value", langRequest.getRawResultSingleLine());
+		
+		//return first element in list when single result queries a list
+		List<String> list = new ArrayList<>();
+		list.add("Value 1");
+		list.add("Value 2");
+		Mockito.when(config.getStringList("test-key")).thenReturn(list);
+		Mockito.when(config.isList("test-key")).thenReturn(true);
+		Assert.assertEquals("Value 1", langRequest.getRawResultSingleLine());
+	}
+	
+	@Test
+	public void canReturnDefaultRawSingleInput() {
+		LangManager langManager = Mockito.mock(LangManager.class);
+		LangConfig config = Mockito.mock(LangConfig.class);
+		LangRequest langRequest = new LangRequest(langManager, Locale.ENGLISH, "not-valid-key", "Nothing Found");
+		
+		//return element found
+		Mockito.when(langManager.getAnyValidLangConfig(Locale.ENGLISH)).thenReturn(Optional.of(config));
+		Mockito.when(config.contains("test-key")).thenReturn(true);
+		Mockito.when(config.getString("test-key", "Nothing Found")).thenReturn("Found Value");
+		Assert.assertEquals("Nothing Found", langRequest.getRawResultSingleLine());
+		
+		//return first element in list when single result queries a list
+		List<String> list = new ArrayList<>();
+		list.add("Value 1");
+		list.add("Value 2");
+		Mockito.when(config.getStringList("test-key")).thenReturn(list);
+		Mockito.when(config.isList("test-key")).thenReturn(true);
+		Assert.assertEquals("Nothing Found", langRequest.getRawResultSingleLine());
+	}
+	
+	@Test
+	public void canReturnRawInput() {
+		LangManager langManager = Mockito.mock(LangManager.class);
+		LangConfig config = Mockito.mock(LangConfig.class);
+		LangRequest langRequest = new LangRequest(langManager, Locale.ENGLISH, "test-key", "Nothing Found");
+		
+		//return element found
+		Mockito.when(langManager.getAnyValidLangConfig(Locale.ENGLISH)).thenReturn(Optional.of(config));
+		Mockito.when(config.contains("test-key")).thenReturn(true);
+		Mockito.when(config.getString("test-key", "Nothing Found")).thenReturn("Found Value");
+		Assert.assertEquals(List.of("Found Value"), langRequest.getRawResult());
+		
+		//return first element in list when single result queries a list
+		List<String> list = new ArrayList<>();
+		list.add("Value 1");
+		list.add("Value 2");
+		Mockito.when(config.getStringList("test-key")).thenReturn(list);
+		Mockito.when(config.isList("test-key")).thenReturn(true);
+		Assert.assertEquals(List.of("Value 1", "Value 2"), langRequest.getRawResult());
+	}
+	
+	@Test
+	public void canReturnDefaultRawInput() {
+		LangManager langManager = Mockito.mock(LangManager.class);
+		LangConfig config = Mockito.mock(LangConfig.class);
+		LangRequest langRequest = new LangRequest(langManager, Locale.ENGLISH, "not-valid-key", "Nothing Found");
+		
+		//return element found
+		Mockito.when(langManager.getAnyValidLangConfig(Locale.ENGLISH)).thenReturn(Optional.of(config));
+		Mockito.when(config.contains("test-key")).thenReturn(true);
+		Mockito.when(config.getString("test-key", "Nothing Found")).thenReturn("Found Value");
+		Assert.assertEquals(List.of("Nothing Found"), langRequest.getRawResult());
+		
+		//return first element in list when single result queries a list
+		List<String> list = new ArrayList<>();
+		list.add("Value 1");
+		list.add("Value 2");
+		Mockito.when(config.getStringList("test-key")).thenReturn(list);
+		Mockito.when(config.isList("test-key")).thenReturn(true);
+		Assert.assertEquals(List.of("Nothing Found"), langRequest.getRawResult());
+	}
+	
+	@Test
+	public void canPickRightConfig() {
+		JavaPlugin plugin = Mockito.mock(JavaPlugin.class);
+		Mockito.when(plugin.namespace()).thenReturn("testplugin");
+		LangManager langManager = LangManager.getInstance(plugin);
+		LangConfig configEng = Mockito.mock(LangConfig.class);
+		LangConfig configGer = Mockito.mock(LangConfig.class);
+		
+		Assert.assertEquals(Optional.empty(), langManager.getAnyValidLangConfig(Locale.ENGLISH));
+		
+		langManager.addLanguage(configEng, Locale.ENGLISH);
+		langManager.addLanguage(configGer, Locale.GERMAN, Locale.GERMANY);
+		
+		Assert.assertEquals(3, langManager.getAllLangs().size());
+		
+		Assert.assertEquals(Optional.of(configEng), langManager.getAnyValidLangConfig(Locale.ENGLISH));
+		Assert.assertEquals(Optional.of(configGer), langManager.getAnyValidLangConfig(Locale.GERMAN));
+		Assert.assertEquals(Optional.of(configGer), langManager.getAnyValidLangConfig(Locale.GERMANY));
+		
+		//returns any config it can find
+		Assert.assertNotEquals(Optional.empty(), langManager.getAnyValidLangConfig(Locale.FRENCH));
+		langManager.setDefaultLang(Locale.ENGLISH);
+		//returns the english one
+		Assert.assertEquals(Optional.of(configEng), langManager.getAnyValidLangConfig(Locale.FRENCH));
+	}
+	
+	@Test
+	public void canReplacePlaceholders() {
+		LangManager langManager = Mockito.mock(LangManager.class);
+		LangConfig config = Mockito.mock(LangConfig.class);
+		LangRequest langRequest = new LangRequest(langManager, Locale.ENGLISH, "test-key", "Nothing Found").replace("%placeholder%","Resolved");
+		
+		//return element found
+		Mockito.when(langManager.getAnyValidLangConfig(Locale.ENGLISH)).thenReturn(Optional.of(config));
+		Mockito.when(config.contains("test-key")).thenReturn(true);
+		Mockito.when(config.getString("test-key", "Nothing Found")).thenReturn("Found Value %placeholder%");
+		Assert.assertEquals(List.of("Found Value Resolved"), langRequest.processRawResult(langRequest.getRawResult()));
+
+	}
+}
